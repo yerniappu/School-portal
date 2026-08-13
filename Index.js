d1870f751deb3db6d870a9d351bce9608caeb2d3
yerniappu <yernibabu.anumalasetty@growtharc.com>
2026-08-13 19:42:16 +0530

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,9 @@
+function login() {
+
+    const studentId = document.getElementById("studentId").value;
+    const password = document.getElementById("password").value;
+
+    if (studentId === "ST001" && password === "1234") {
+        alert
+    }
+}
